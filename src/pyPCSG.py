30408f7f686a2f42ffd1b47163591d5a9382a946
Steve Kelly <kd2cca@gmail.com>
2013-07-06 19:42:30 -0500

    think we can go up from here, will start being more git-diligent

--- src/pyPCSG.py
+++ src/pyPCSG.py
@@ -14,27 +14,26 @@ class element(object):
 	def __init__(self, name = ""):
 		self.type = "element"
 		self.name = name
+		self.construction = []
 
 class operation(object):
 	def __init__(self,name = "", elements = []):
 		self.type = "operation"
 		self.name = name
-		self.elements = []
-		for elt in elements:
-			if elt.type == self.type and elt.name == self.name:
-				self.elements += elt.elements
-			else:
-				self.elements += [elt]
+		self.elements = elements
+		
 class point(element):
-	def __init__(self):
+	def __init__(self, location):
 		element.__init__(self, name = "point")
-		self.location = [0,0,0]
+		self.location = location
+		self.construction = [self.location]
 		
 class primitive(element):
 	def __init__(self, name = ""):
 		element.__init__(self, name)
 		self.location = [0,0,0]
 		self.center = [0,0,0]
+		self.construction = []
 	def __add__(self, other):
 		return union([self, other])
 	def __sub__(self, other):
@@ -60,42 +59,50 @@ class intersection(operation):
 	def __init__(self,elements):
 		operation.__init__(self, name = "intersection", elements = elements)
 
+class extrude(operation):
+	def __init__(self, elements, length):
+		operation.__init__(self, name = "extrude", elements = elements)
+		self.length = length
+
+class square(primitive):
+	def __init__(self, size = [0,0,0]):
+		primitive.__init__(self, name = "square")
+		self.size = size
+		self.construction = [point([0,0]), point([size[0],0]), point([0,size[1]]), point([size[0], size[1]])]		
+
 class cube(primitive):
-	def __init__(self,size = [0,0,0]):
+	def __init__(self, size = [0,0,0]):
 		primitive.__init__(self, name = "cube")
 		self.size = size
-		
+		self.construction = [extrude(square(size), size[2])]
+
 class cylinder(primitive):
 	def __init__(self, radius = 0, height = 0):
 		primitive.__init__(self, name = "cylinder")
 		self.radius = radius
 		self.height = height
+		self.construction = []
 
 class sphere(primitive):
 	def __init__(self, radius = 0):
 		primitive.__init__(self, name = "sphere")
 		self.radius = radius
+		self.construction = []
 
-class object(object):
-	def __init__(self, parameters = [], construction = [], requires = []):	
-		self.parameters = parameters
-		self.construction = construction
-		self.requires = requires
-	def export(self,str):
-		file = open(str, 'w')
-		ext = os.path.splitext(str)[1]
-		if ext == ".json":
-			jsonpickle.set_encoder_options('simplejson', sort_keys=False, indent = 4)
-			file.write(jsonpickle.encode(self, unpicklable=False))
+def export(obj, path):
+	file = open(path, 'w')
+	ext = os.path.splitext(path)[1]
+	if ext == ".json":
+		jsonpickle.set_encoder_options('simplejson', sort_keys=True, indent = 4)
+		file.write(jsonpickle.encode(obj, unpicklable=False))
 		
 
 param = parameter("a param")	
-c = cube([param,0,3])
+c = cube([0,0,3])
 a = cube([1,1,1])
 b = cube([10,10,10])
 k = cylinder(radius = 1, height = 10)
 sphere = sphere(radius = 2)
 u = c+a+b-k
 i = intersection([u,a])
-myObj = object(parameters = param, construction = i)
-myObj.export("../tests/simple_test.json")
+export(i,"../tests/simple_test.json")
